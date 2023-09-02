ad00b0a332984c0ce76b3e4ddc6d1a911b926e9c
Alex Costa <alexandre.costa@code.berlin>
2023-09-02 19:00:10 +0200

    country routes

---
+++ prisma/migrations/20230902134737_remove_languages_country_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `countryId` on the `Language` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Language" DROP COLUMN "countryId";
--- src/routes/countries/index.ts
+++ src/routes/countries/index.ts
@@ -2,19 +2,161 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { prisma } from '../../index';
 
+interface CreateCountryRequest {
+  id: number;
+  country: string;
+  code: string;
+  currencyId: number;
+  languages: number[];
+}
+
+interface LanguageIds {
+  languages: number[];
+}
+
 const router = express.Router();
 
-router.get(`/`, async (req: Request, res: Response) => {
-  const countries = await prisma.country.findMany()
-  res.json(countries)
-})
-
-router.get(`/:id`, async (req: Request, res: Response) => {
-  const { id } = req.params
-  const countries = await prisma.country.findUnique({
-    where: { id: Number(id) }
+router.route(`/`)
+  .get(async (req: Request, res: Response) => {
+    const countries = await prisma.country.findMany()
+    res.json(countries)
   })
-  res.json(countries)
-})
+  .post(async (req: Request<{}, {}, CreateCountryRequest>, res: Response) => {
+    const { country, code, currencyId, languages = [] } = req.body
+    const result = await prisma.country.create({
+      data: {
+        code,
+        country,
+        currency: { connect: { id: currencyId } },
+        languages: { connect: languages.map((languageId) => ({ id: languageId })) }
+      },
+      select: {
+        country: true,
+        code: true,
+        currency: { select: { currency: true } },
+        languages: { select: { language: true } }
+      }
+    })
+    res.json(result)
+  })
+
+
+router.route(`/:id`)
+  .get(async (req: Request, res: Response) => {
+    const { id } = req.params
+    const countries = await prisma.country.findUnique({
+      where: { id: Number(id) }
+    })
+    res.json(countries)
+  })
+  .put(async (req: Request, res: Response) => {
+    const { id } = req.params
+    const data = req.body
+    const country = await prisma.country.update({
+      where: {
+        id: Number(id)
+      },
+      data: data
+    })
+    res.json(country)
+  })
+  .delete(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const deletedCountry = await prisma.country.delete({
+      where: {
+        id: Number(id)
+      }
+    })
+    res.json(deletedCountry)
+  })
+
+router.route('/:id/languages')
+  .get(async (req: Request, res: Response) => {
+    const { id } = req.params
+    const languages = await prisma.country.findUnique({
+      where: {
+        id: Number(id)
+      },
+      select: {
+        languages: true
+      }
+    })
+    res.json(languages)
+  })
+  .post(async (req: Request<{ id: number }, {}, LanguageIds>, res: Response) => {
+    const { id } = req.params
+    const { languages: addedLanguages } = req.body
+
+    const removedLanguages = await prisma.country.findUnique({
+      where: { id: Number(id) },
+      select: { languages: true }
+    })
+
+    // Disconnect the existing languages
+    await prisma.country.update({
+      where: { id: Number(id) },
+      data: {
+        languages: {
+          disconnect: removedLanguages?.languages.map(lang => ({ id: lang.id }))
+        }
+      }
+    })
+
+    // Connect new languages
+    await prisma.country.update({
+      where: { id: Number(id) },
+      data: {
+        languages: {
+          connect: addedLanguages.map(id => ({ id: id }))
+        }
+      }
+    })
+
+    const updatedCountry = await prisma.country.findUnique({
+      where: { id: Number(id) },
+      include: {
+        languages: true
+      }
+    })
+
+    res.json(updatedCountry)
+  })
+  .put(async (req: Request<{ id: number }, {}, LanguageIds>, res: Response) => {
+    const { id } = req.params
+    const { languages: addedLanguages } = req.body
+
+    const updatedCountry = await prisma.country.update({
+      where: { id: Number(id) },
+      data: {
+        languages: {
+          connect: addedLanguages.map(languageId => ({ id: languageId }))
+        }
+      },
+      include: {
+        languages: true
+      }
+    })
+    res.json(updatedCountry)
+  })
+  .delete(async (req: Request<{ id: number }, {}, LanguageIds>, res: Response) => {
+    const { id } = req.params
+    const { languages: removedLanguages } = req.body
+
+    const updatedCountry = await prisma.country.update({
+      where: { id: Number(id) },
+      data: {
+        languages: {
+          disconnect: removedLanguages.map(languageId => ({ id: languageId }))
+        }
+      },
+      include: {
+        languages: true
+      }
+    })
+    res.json(updatedCountry)
+  })
+
+
+
 
 export default router;
