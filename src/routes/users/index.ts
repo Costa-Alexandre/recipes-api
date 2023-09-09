8a0099f325a3af67cb915e276c446d9c2f6980ad
Alex Costa <alexandre.costa@code.berlin>
2023-09-09 17:29:16 +0200

    remove salt from separate field

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import { prisma } from '../../index';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 
 const router = express.Router();
@@ -20,6 +22,22 @@ router.route(`/`)
   .get(async (req: Request, res: Response) => {
     const users = await prisma.user.findMany()
     res.json(users)
+  })
+  .post(async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+      console.log(salt, hashedPassword);
+    } catch (error) {
+      console.log(error);
+    }
+    return res.status(200).json({ message: 'User created' })
+  })
+
+router.route(`/login`)
+  .post(async (req: Request, res: Response) => {
+
   })
 
 
